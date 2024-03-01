ab5856d117ef835255f2cd23836291734d90b077
Kelly <kellyltran04@gmail.com>
2024-03-01 16:58:02 -0500

    Implemented the user registration feature

--- CarRentalSystem/urls.py
+++ CarRentalSystem/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from frontend import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('register', views.register, name='register'),
 ]
